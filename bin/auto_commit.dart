49441e610ee69413e2af7bb7aaccf30364cb6b76
Cals <cals@CalsdeMac-mini.local>
2024-11-15 10:21:46 +0800

    fix: update executable name from 'auto commit' to 'flit' and ensure help is shown when no arguments are provided

--- bin/auto_commit.dart
+++ bin/auto_commit.dart
@@ -3,10 +3,11 @@ import 'package:auto_commit/commit_command.dart';
 import 'package:auto_commit/config_command.dart';
 
 Future<void> main(List<String> arguments) async {
-  var executable = 'auto commit';
+  var executable = 'flit';
   var description = 'AI-powered Conventional Commits Generator';
   var runner = CommandRunner(executable, description)
     ..addCommand(CommitCommand())
     ..addCommand(ConfigCommand());
+  if (arguments.isEmpty) arguments = ['--help'];
   await runner.run(arguments);
 }
