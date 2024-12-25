cd1e95ce18744bb7c2fa1bdf869aabffb445e5c3
Cals <cals@CalsdeMac-mini.local>
2024-12-25 17:48:21 +0800

    feat(generator): add JSON schema for commit messages
    Implemented _formatResponseMessage and _getResponseFormat to handle and validate commit messages in JSON format.
    
    Upgrade to this version to leverage structured commit messages in your workflow.

--- lib/generator.dart
+++ lib/generator.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:auto_commit/config.dart';
 import 'package:openai_dart/openai_dart.dart';
 
@@ -16,12 +18,64 @@ class Generator {
     var request = CreateChatCompletionRequest(
       model: ChatCompletionModel.modelId(config.model),
       messages: [systemMessage, userMessage],
+      responseFormat: _getResponseFormat(),
     );
     try {
       var response = await client.createChatCompletion(request: request);
-      return response.choices.first.message.content ?? '';
+      return _formatResponseMessage(response.choices.first.message.content);
     } finally {
       client.endSession();
     }
+  }
+
+  static String _formatResponseMessage(String? content) {
+    if (content == null) return '';
+    try {
+      var json = jsonDecode(content);
+      var type = json['commitType'];
+      var scope = json['scope'];
+      var description = json['description'];
+      var body = json['body'];
+      var footer = json['footer'];
+      var buffer = StringBuffer();
+      buffer.write(type);
+      if (scope != null) buffer.write('($scope)');
+      buffer.write(': ');
+      buffer.write(description);
+      if (body != null) buffer.write('\n\n$body');
+      if (footer != null) buffer.write('\n\n$footer');
+      return buffer.toString();
+    } catch (error) {
+      return content;
+    }
+  }
+
+  static ResponseFormat _getResponseFormat() {
+    var properties = {
+      'commitType': {'type': 'string'},
+      'scope': {
+        'type': ['string', 'null']
+      },
+      'description': {'type': 'string'},
+      'body': {
+        'type': ['string', 'null']
+      },
+      'footer': {
+        'type': ['string', 'null']
+      },
+    };
+    var schema = {
+      'type': 'object',
+      'properties': properties,
+      'additionalProperties': false,
+      'required': properties.keys.toList(),
+    };
+    var jsonSchemaObject = JsonSchemaObject(
+      name: 'commit_message',
+      description: 'Conventional Commits style commit message',
+      strict: true,
+      schema: schema,
+    );
+    return ResponseFormat.jsonSchema(jsonSchema: jsonSchemaObject);
   }
 }
